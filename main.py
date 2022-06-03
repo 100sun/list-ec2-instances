44cf7eb2b4daedac578f7bc85572450dfeded507
100sun <sofia.100@kakaostyle.com>
2022-06-03 12:22:26 +0900

    Add xlsx to use hyperlink

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+properties = [
+    "InstanceId",
+    "Tag)Name",
+    "InstanceType",
+    "InstanceState",
+    "LaunchTime",
+    "KeyPair",
+    "VpcName",
+    "SubnetId",
+    "VpcId",
+    "PrivateIpAddress",
+    "PublicIpAddress",
+    "Tag)Service",
+    "Tag)autoscaling:groupName",
+    "Tag)ec2:fleet-id",
+    "Tag)cloudformation:logical-id",
+    "Tag)cloudformation:stack-id",
+    "Tag)cloudformation:stack-name",
+    "Tag)etc.."
+]
+
+
 def get_ec2_instances(account_name):
     import boto3
+    import json
 
     boto3.setup_default_session(profile_name=f"{account_name}", region_name='ap-northeast-2')
     ec2_client = boto3.client('ec2')
@@ -11,6 +34,8 @@ def get_ec2_instances(account_name):
         return refined_tags
 
     def get_vpc_name(vpc_id):
+        if not vpc_id:
+            return
         for vpc_tag in ec2_client.describe_vpcs(VpcIds=[vpc_id])['Vpcs'][0]['Tags']:
             if vpc_tag['Key'] == 'Name':
                 return vpc_tag['Value']
@@ -26,18 +51,18 @@ def get_ec2_instances(account_name):
             instance["State"]["Name"],
             instance["LaunchTime"],
             instance.get("KeyName", ""),
-            get_vpc_name(instance["VpcId"]),
-            instance["SubnetId"],
-            instance["VpcId"],
-            instance["PrivateIpAddress"],
-            instance.get("PublicIpAddress"),
+            get_vpc_name(instance.get("VpcId", "")),
+            instance.get("SubnetId", ""),
+            instance.get("VpcId", ""),
+            instance.get("PrivateIpAddress", ""),
+            instance.get("PublicIpAddress", ""),
             instance_tags.pop("Service", ""),
             instance_tags.pop("aws:autoscaling:groupName", ""),
             instance_tags.pop("aws:ec2:fleet-id", ""),
             instance_tags.pop("aws:cloudformation:logical-id", ""),
             instance_tags.pop("aws:cloudformation:stack-id", ""),
             instance_tags.pop("aws:cloudformation:stack-name", ""),
-            instance_tags,
+            json.dumps(instance_tags),
         ])
 
     return instances_info
@@ -45,34 +70,43 @@ def get_ec2_instances(account_name):
 
 def write_to_csv(account_name):
     import csv
-    with open(f'ec2_instances_{account_name}.csv', 'w', encoding='UTF8', newline='') as f:
+    with open(f'{account_name}.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
 
-        properties = [
-            "InstanceId",
-            "Tag)Name",
-            "InstanceType",
-            "InstanceState",
-            "LaunchTime",
-            "KeyPair",
-            "VpcName",
-            "SubnetId",
-            "VpcId",
-            "PrivateIpAddress",
-            "PublicIpAddress",
-            "Tag)Service",
-            "Tag)autoscaling:groupName",
-            "Tag)ec2:fleet-id",
-            "Tag)cloudformation:logical-id",
-            "Tag)cloudformation:stack-id",
-            "Tag)cloudformation:stack-name",
-            "Tag)etc.."
-        ]
         writer.writerow(properties)
         writer.writerows(get_ec2_instances(account_name))
 
 
+def write_to_xlsx(account_name):
+    import xlsxwriter
+
+    workbook = xlsxwriter.Workbook(f"{account_name}.xlsx", {'remove_timezone': True})
+    worksheet = workbook.add_worksheet(f"{account_name}")
+
+    instances_info = get_ec2_instances(account_name)
+    for i in range(len(properties)):
+        worksheet.write(0, i, properties[i])
+
+    for i in range(0, len(instances_info)):
+        for j in range(len(instances_info[i])):
+            if j == 0:
+                worksheet.write_url(i + 1, j,
+                                    f'https://ap-northeast-2.console.aws.amazon.com/ec2/v2/home?region=ap-northeast-2#InstanceDetails:instanceId={instances_info[i][j]}',
+                                    string=instances_info[i][j])
+            # elif j == 4:
+            #     worksheet.write_datetime(i + 1, j, instances_info[i][j])
+            elif j == 8:
+                worksheet.write_url(i + 1, j,
+                                    f'https://ap-northeast-2.console.aws.amazon.com/vpc/home?region=ap-northeast-2#VpcDetails:VpcId={instances_info[i][j]}',
+                                    string=instances_info[i][j])
+            else:
+                worksheet.write(i + 1, j, instances_info[i][j])
+
+    workbook.close()
+
+
 if __name__ == "__main__":
     accounts = ["croquis-main", "zigzag-main", "zigzag-alpha"]
-    for account in accounts[:1]:
-        write_to_csv(account)
+    write_to_xlsx(accounts[0])
+    # write_to_xlsx(accounts[1])
+    # write_to_xlsx(accounts[2])
